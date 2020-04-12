3b813ace416de6d42906d3dbcd8954bed34d50a4
ryan <rbed23@gmail.com>
2020-04-12 07:41:19 +0300

    added comments and URL input

--- py-gfm-donations-analysis.py
+++ py-gfm-donations-analysis.py
@@ -9,11 +9,16 @@ def create_gfm_gateway_call(gfmcn, limit, offset):
     <type gfmcn> str
     <desc gfmcn> url campaign designation
         ex URL = "habitat-for-the-pathak-family"
-
+    <type limit> int
+    <desc limit> maximum number of donations to return in single call
+        *** hard set at 100 by GFM Gateway***
+    <type offset> int
+    <desc offset> used to retrieve the next set of donations, if >100 total
 
     <<type gwurl>> str
     <<desc gwurl>> url used to parse info from the GFM gateway api
-        ex URL = "https://gateway.gofundme.com/web-gateway/v1/feed/habitat-for-the-pathak-family/donations?limit=100&sort=recent"
+        ex URL = "https://gateway.gofundme.com/web-gateway/v1/feed/habitat-for-the-pathak-family/
+        donations?limit=100&sort=recent"
     '''
     SORT_DIRECTION = 'recent'   # most recent donations listed first
 
@@ -28,8 +33,8 @@ def get_donations_list(gfmcn):
     <type gfmcn> str
     <desc gfmcn> url campaign designation
 
-    <<type json_response>> list
-    <<desc json_response>> detailed dicts individual donation information
+    <<type full_list>> list
+    <<desc full_list>> detailed dicts of individual donation information
     '''
     DONATIONS_LIMIT = 100     # maximum returned donations in a single call (GFM limit)
     full_list = []
@@ -55,9 +60,8 @@ def check_gfm_url(url):
     <<type gfm_campaign_name>> str
     <<desc gfm_campaign_name>> url campaign designation
     '''
-
-    url_elements = url.split('/')
-    if 'gofundme.com' in url:
+    if 'gofundme.com' in url: # validates url input
+        url_elements = url.split('/')
         if 'https' in url:
             gfm_campaign_name = url_elements[4]
         else:
@@ -67,25 +71,25 @@ def check_gfm_url(url):
         print("not a valid gofundme.com URL")
         print("  :ex: gofundme.com/f/[campaign-name-url]")
         url_input = input("  :try again: ")
-        return check_gfm_url(url_input)
+        return check_gfm_url(url_input) # recursive url check
 
 def main(url):
     print(f"Called script using <{url}> as input...")
 
     #get the GoFundMe Campaign name designation
-    print(url)
     gfm_name = check_gfm_url(url)
-    print(gfm_name)
+
     # get clean list of donations from the GFM Gateway
     cleaned_list = get_donations_list(gfm_name)
 
+    # setup output information
     donations = [x["amount"] for x in cleaned_list]
     anondonations = [x["amount"] for x in cleaned_list\
          if x['is_anonymous']]
     nonanondonations = [x["amount"] for x in cleaned_list\
          if not x['is_anonymous']]
     big_donors = [{'name': x['name'], 'amt': x['amount']} for x in cleaned_list\
-         if x['amount'] >= 100]
+         if x['amount'] >= 500]
 
     print(f"    Number of donations: {len(donations)}\n\
     Total Amount Donated: ${sum(donations)}\n\
@@ -108,11 +112,10 @@ def main(url):
         print(f"{each['name'].rjust(25)}: ${str(int(each['amt'])).rjust(7)}")
 
 if __name__ == "__main__": 
-    URL = "https://www.gofundme.com/f/habitat-for-the-pathak-family/"    
-
     if len(sys.argv) == 2:
         # If there are keyword arguments
         main(sys.argv[1])
     elif len(sys.argv) == 1:
-        # If no keywork arguments
-        main(URL)
+        # If no keyword arguments
+        url_input = input("Please enter a valid gofundme.com URL:\n::  ")
+        main(url_input)
